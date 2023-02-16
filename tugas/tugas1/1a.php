327514cc9a96ceca83bb557c4c4331c84c88a970
DaffaRiyadi <daffariyadi2003@gmail.com>
2023-02-16 21:09:31 +0700

    first commit

---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,26 @@
+<?php 
+$variable1 = "Topi"; 
+$variable2 = "Bundar";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>
+        <i>
+            "<?php echo "$variable1";?> 
+            saya 
+            <?php echo "$variable2";?>, 
+            <?php echo "$variable2";?> 
+            saya 
+            <?php echo "$variable1";?>"
+        </i>
+    </h2>
+</body>
+</html>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,20 @@
+<?php $nim = 20; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        Aku adalah angka <b><?php echo $nim; ?></b><br>
+        Jika aku dikali 5,maka aku sekarang menjadi <b><?php echo $nim * 5 ?></b><br>
+        Jika aku dibagi 2,maka aku sekarang menjadi <b><?php echo 100 / 2 ?></b><br>
+        Jika aku ditambah 75,maka aku sekarang menjadi <b><?php echo 50 + 75 ?></b><br>
+        Jika aku dikurang 20,maka aku serang menjadi <b><?php echo 125 - 20 ?></b><br>
+    </p>
+</body>
+</html>
