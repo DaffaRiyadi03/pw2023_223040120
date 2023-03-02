063cde3cf317d82b348fff3af573386f7847278c
DaffaRiyadi03 <daffariyadi100@gmail.com>
2023-03-02 16:10:08 +0700

    mengerjakan tugas2

--- kuliah/pertemuan3/latihan1.php
+++ kuliah/pertemuan3/latihan1.php
@@ -1,10 +1,8 @@
 
 <?php 
-echo "Mulai <br>";
 $nilai_awal = 1;
-while($nilai_awal <= 5){
-echo "Hello World <br>";
-$nilai_awal += 1;
+while($nilai_awal <= 10){
+echo "Angkot no.$nilai_awal beroperasi dengan baik. <br>";
+$nilai_awal += 1 ;
 }
-echo "Selesai<br>";
 ?>
